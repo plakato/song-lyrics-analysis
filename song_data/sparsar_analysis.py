1f518dc7d873bb82ebddc2fa1c6cab10330a3474
Patricia Brezinova <patricia@brezinovi.sk>
2020-04-16 21:23:54 +0200

    added csv summary of rhyme scheme

--- song_data/sparsar_analysis.py
+++ song_data/sparsar_analysis.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
+import ast
 import json
 import os
-import subprocess
+import xml.etree.ElementTree as ET
 
-
-filename = 'data/100lyrics_cleaned.json'
 
 def create_sparsar_input_file_from_song(song, output_filename):
     with open(output_filename, 'w+') as output:
@@ -15,10 +14,10 @@ def create_sparsar_input_file_from_song(song, output_filename):
             # line = line.replace(u'–', '-')
             # line = line.replace('\'', '\'')
             # line = line.replace('\'', '\'')
-            output.write(line.encode('utf-8') + '\n')
+            output.write(line + '\n')
 
-def main():
-    os.environ["PYTHONIOENCODING"] = "utf-8"
+
+def run_sparsar_for_files(filename):
     with open(filename) as input:
         data = json.load(input)
         os.chdir("./sparsar_experiments")
@@ -28,6 +27,50 @@ def main():
                 print(song_file)
                 create_sparsar_input_file_from_song(song, song_file)
                 os.system("./sparsar_man loadallouts -- \"" + song_file + "\"")
+
+
+def extract_rhymes_to_csv(filename):
+    inputfile = 'sparsar_experiments/outs/\'' + filename + '\'_phon.xml'
+    outputfile = 'sparsar_experiments/rhymes/\'' + filename + '\'.csv'
+    tree = ET.parse(inputfile)
+
+    root = tree.getroot()
+    # Get rhyme scheme.
+    schemes = root[2][0].attrib['Different_Rhyme_Schemes']
+    schemes = schemes.replace('[', '').replace(']', '').split(',')
+    print(schemes)
+    # Remove title and author rhyme letters.
+    i = 0
+    with open(outputfile, 'w+') as output:
+        output.write('Rhyme Scheme Letter;Line Number;Lyrics;Phonetic '
+                     'Transcription\n')
+        for stanza in root[0]:
+            for line in stanza[1]:
+                words = []
+                phons = []
+                for word in line.attrib['line_syllables'].split(']'):
+                    if word == '':
+                        continue
+                    parts = word.replace('[', '').split('/')
+                    words.append(parts[0].replace(',', ''))
+                    phons.append(parts[1].replace(',', '_'))
+                output.write('{0};{1};{2};{3}\n'.format(schemes[i],
+                                                        line.attrib[
+                    'no'],
+                                                       ' '.join(
+                    words), ' '.join(phons)))
+                i += 1
+        while i < len(schemes):
+            output.write(schemes[i] + '\n')
+            i += 1
+
+
+def main():
+    filename = 'data/100lyrics_cleaned.json'
+    os.environ["PYTHONIOENCODING"] = "utf-8"
+    # run_sparsar_for_files(filename)
+    extract_rhymes_to_csv('Cruising USA Lyrics.txt')
+
 
 if __name__ == '__main__':
     main()
