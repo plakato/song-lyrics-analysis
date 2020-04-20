c3327373dbf80746c94eb9f196ca4e29470347de
Patricia Brezinova <patricia@brezinovi.sk>
2020-04-20 12:10:50 +0200

    my own rhyme scheme detector - only perfect rhymes

---
+++ song_data/rhyme_scheme_detector.py
@@ -0,0 +1,48 @@
+import os
+import string
+import xml.etree.ElementTree as ET
+
+
+def load_lines(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    words = []
+    phons = []
+    for stanza in root[0]:
+        for line in stanza[1]:
+            words_line = []
+            phons_line = []
+            # print(line.attrib['line_syllables'].split(']'))
+            for word in line.attrib['line_syllables'].split(']'):
+                if word == '':
+                    continue
+                word = word.replace('[', '').replace(',', '')
+                parts = word.split('/')
+                words_line.append(parts[0])
+                phons_line.append(parts[1].split(','))
+            words.append(words_line)
+            phons.append(phons_line)
+    return words, phons
+
+
+def find_perfect_rhymes(lines):
+    letters = list(string.ascii_letters)
+    mapping = {}
+    scheme = []
+    for line in lines:
+        last2phons = '_'.join(line[-1][-1].split('_')[-2:])
+        if last2phons not in mapping:
+            letter = letters.pop(0)
+            mapping[last2phons] = letter
+        else:
+            letter = mapping[last2phons]
+        scheme.append(letter)
+    return scheme
+
+
+words, phons = load_lines('sparsar_experiments/outs/\'Ask Me '
+                          'Lyrics.txt\'_phon.xml')
+scheme = find_perfect_rhymes(phons)
+i = 0
+for i in range(len(scheme)):
+    print('{0} {1}'.format(scheme[i], ' '.join(words[i])))
