b96a8e6044be1a8caab223e28aeeeb22fea8026f
Patricia Brezinova <patricia@brezinovi.sk>
2020-06-29 14:45:09 +0200

    with more logs

--- song_data/learning/rep_matrix_classifier_cnn.py
+++ song_data/learning/rep_matrix_classifier_cnn.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-import random
-from os.path import isfile, join
+import sys
 
 import numpy as np
 import tensorflow as tf
 
-from tensorflow.python import keras
-import sklearn
-from sklearn.model_selection import train_test_split
+# from tensorflow.python import keras
 
 
 # Custom filter
-from song_data.learning.generator import RhymeMatrixImageBatchGenerator
+from generator import RhymeMatrixImageBatchGenerator
 n_filters = 1
 
 
@@ -23,22 +20,23 @@ def cnn_filter(shape, dtype=None):
                   ])
     print(shape, f.shape)
     assert f.shape == shape
-    return keras.backend.variable(f, dtype='float32')
+    return tf.keras.backend.variable(f, dtype='float32')
 
 
 # The neural network model
 class Network(tf.keras.Sequential):
     def __init__(self, args):
         super().__init__()
-        regularizer = tf.keras.regularizers.l2(1e-4)
+        # regularizer = tf.keras.regularizers.l2(1e-4)
         inputs = tf.keras.layers.Input(shape=[None, None, 1])
-        x = tf.keras.layers.Conv2D(n_filters,  3, strides=2,
+        conv = tf.keras.layers.Conv2D(n_filters,  3, strides=2,
                                             padding='valid',
                                             kernel_initializer=cnn_filter,
-                                            trainable=False,  # Don't change the filter.
+                                            trainable=True,  # Don't change the filter.
                                             # kernel_regularizer=regularizer,
                                             data_format="channels_last",
-                                            activation=tf.nn.relu)(inputs)
+                                            activation=tf.nn.relu)
+        x = conv(inputs)
         x = tf.keras.layers.Dropout(0.1)(x)
         x = tf.keras.layers.BatchNormalization()(x)
 
@@ -57,12 +55,28 @@ class Network(tf.keras.Sequential):
         print(self.model.summary())
 
     def train(self, train_generator, val_generator, args):
+        print_weights = tf.keras.callbacks.LambdaCallback(on_epoch_end=lambda batch,
+                                                       logs: self.print_logs())
         history = self.model.fit(train_generator,
                                  epochs=args.epochs,
                                  verbose=1,
-                                 callbacks=[tf.keras.callbacks.ModelCheckpoint(args.logdir, monitor='val_loss', save_best_only=True, verbose=1)],
+                                 callbacks=[tf.keras.callbacks.ModelCheckpoint(args.logdir, monitor='val_loss', save_best_only=True, verbose=1),
+                                           print_weights],
                                  validation_data=val_generator)
         return history
+
+    def print_logs(self):
+        print(self.model.layers[1].get_weights())
+        print(self.model.output)
+
+        # # With and without the activation
+        # fun_without = tf.keras.backend.function([self.model.layers[0].input], [self.model.layers[0].output])
+        # fun_with = tf.keras.backend.function([self.model.layers[0].input], [self.model.layers[1].output])
+        # # Input
+        # x_inp = np.reshape([], (1, 28, 28, 1))
+        # # Output
+        # layer_output_without = fun_without([x_inp])[0]
+        # layer_output_with = fun_with([x_inp])[0]
 
 
 def load_images_for_dir(dir):
